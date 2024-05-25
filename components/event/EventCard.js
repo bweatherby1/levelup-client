f2329c2fc51102cbb03f2f07c6d47e97977d9bc9
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-05-24 22:57:37 -0500

    added games and events to navbar and made their pages

---
+++ components/event/EventCard.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const EventCard = ({
+  game, //
+  description,
+  date,
+  time,
+  organizer,
+}) => (
+  <Card className="text-center">
+    <Card.Header>{description}</Card.Header>
+    <Card.Body>
+      <Card.Title>By: {organizer}</Card.Title>
+      <Card.Text>{date}, {time}</Card.Text>
+    </Card.Body>
+    <Card.Footer className="text-muted">{game}</Card.Footer>
+  </Card>
+);
+
+EventCard.propTypes = {
+  game: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  date: PropTypes.number.isRequired,
+  time: PropTypes.number.isRequired,
+  organizer: PropTypes.string.isRequired,
+};
+
+export default EventCard;
