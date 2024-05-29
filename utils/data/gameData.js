2d5ffe341ca9fb99b7cf026faad8e28170291bb2
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-05-28 19:59:35 -0500

    fixed events

--- utils/data/gameData.js
+++ utils/data/gameData.js
@@ -7,18 +7,18 @@ const getGames = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-const createGame = (game) => fetch(`${clientCredentials.databaseURL}/games`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(game),
-})
-  .then((response) => response.json())
-  .catch((error) => {
-    console.error('Error creating game:', error);
-    throw error; // Re-throw the error to propagate it to the caller
-  });
+const createGame = (game) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/games`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(game),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
 
 const getGameTypes = () => new Promise((resolve, reject) => {
   fetch(`${clientCredentials.databaseURL}/gametypes`, {})
